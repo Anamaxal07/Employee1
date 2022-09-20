b0fcaa83bd112d3b8963511b447364876f4dbecd
Anamaxal07 <neilbryanfranco02@gmail.com>
2022-09-20 15:22:49 +0800

    Employee VB

---
+++ Try/Form1.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtlastname = New System.Windows.Forms.TextBox()
+        Me.cmbgender = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtfirsttname = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtmi = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtaddress = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.cmbposition = New System.Windows.Forms.ComboBox()
+        Me.txtcontact = New System.Windows.Forms.TextBox()
+        Me.lblempno = New System.Windows.Forms.Label()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(55, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Employee No:"
+        '
+        'txtlastname
+        '
+        Me.txtlastname.Location = New System.Drawing.Point(58, 101)
+        Me.txtlastname.Name = "txtlastname"
+        Me.txtlastname.Size = New System.Drawing.Size(164, 20)
+        Me.txtlastname.TabIndex = 1
+        '
+        'cmbgender
+        '
+        Me.cmbgender.FormattingEnabled = True
+        Me.cmbgender.Items.AddRange(New Object() {"Male", "Female"})
+        Me.cmbgender.Location = New System.Drawing.Point(58, 214)
+        Me.cmbgender.Name = "cmbgender"
+        Me.cmbgender.Size = New System.Drawing.Size(121, 21)
+        Me.cmbgender.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(55, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Last Name"
+        '
+        'txtfirsttname
+        '
+        Me.txtfirsttname.Location = New System.Drawing.Point(228, 101)
+        Me.txtfirsttname.Name = "txtfirsttname"
+        Me.txtfirsttname.Size = New System.Drawing.Size(162, 20)
+        Me.txtfirsttname.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(225, 85)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "First Name"
+        '
+        'txtmi
+        '
+        Me.txtmi.Location = New System.Drawing.Point(396, 101)
+        Me.txtmi.Name = "txtmi"
+        Me.txtmi.Size = New System.Drawing.Size(35, 20)
+        Me.txtmi.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(393, 85)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(19, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "MI"
+        '
+        'txtaddress
+        '
+        Me.txtaddress.Location = New System.Drawing.Point(58, 158)
+        Me.txtaddress.Name = "txtaddress"
+        Me.txtaddress.Size = New System.Drawing.Size(373, 20)
+        Me.txtaddress.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(55, 142)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(45, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Address"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(57, 198)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(42, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Gender"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(184, 197)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(64, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Contact No."
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(322, 198)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(44, 13)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "Position"
+        '
+        'cmbposition
+        '
+        Me.cmbposition.FormattingEnabled = True
+        Me.cmbposition.Items.AddRange(New Object() {""})
+        Me.cmbposition.Location = New System.Drawing.Point(323, 214)
+        Me.cmbposition.Name = "cmbposition"
+        Me.cmbposition.Size = New System.Drawing.Size(108, 21)
+        Me.cmbposition.TabIndex = 13
+        '
+        'txtcontact
+        '
+        Me.txtcontact.Location = New System.Drawing.Point(185, 214)
+        Me.txtcontact.Name = "txtcontact"
+        Me.txtcontact.Size = New System.Drawing.Size(132, 20)
+        Me.txtcontact.TabIndex = 15
+        '
+        'lblempno
+        '
+        Me.lblempno.AutoSize = True
+        Me.lblempno.Location = New System.Drawing.Point(134, 42)
+        Me.lblempno.Name = "lblempno"
+        Me.lblempno.Size = New System.Drawing.Size(0, 13)
+        Me.lblempno.TabIndex = 16
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.ListView1.GridLines = True
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(58, 253)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(373, 169)
+        Me.ListView1.TabIndex = 17
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "ID"
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Last Name"
+        Me.ColumnHeader2.Width = 109
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "First Name"
+        Me.ColumnHeader3.Width = 120
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Position"
+        Me.ColumnHeader4.Width = 158
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(483, 450)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.lblempno)
+        Me.Controls.Add(Me.txtcontact)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.cmbposition)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtaddress)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtmi)
+        Me.Controls.Add(Me.txtfirsttname)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.cmbgender)
+        Me.Controls.Add(Me.txtlastname)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Employee Information"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtlastname As TextBox
+    Friend WithEvents cmbgender As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtfirsttname As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtmi As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtaddress As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents cmbposition As ComboBox
+    Friend WithEvents txtcontact As TextBox
+    Friend WithEvents lblempno As Label
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents ColumnHeader4 As ColumnHeader
+End Class
---
+++ Try/Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Data
+Imports Npgsql
+Public Class Form1
+
+    Dim lv As ListViewItem
+    Dim Selected As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PopListview()
+    End Sub
+
+    Private Sub PoplistView()
+        ListView1.Clear()
+        With ListView1
+            .View = View.Details
+            .GridLines = True
+            .Columns.Add("ID", 40)
+            .Columns.Add("Last Name", 110)
+            .Columns.Add("First Name", 110)
+            .Columns.Add("Position", 110)
+        End With
+
+        openCon()
+        sql = "Select * from tbl_empinfo"
+        cmd = New NpgsqlCommand(sql, cn)
+        dr = cmd.ExecuteReader()
+
+        Do While dr.Read() = True
+            lv = New ListViewItem(dr("empno").ToString)
+            lv.SubItems.Add(dr("emplastname"))
+            lv.SubItems.Add(dr("empfirstname"))
+            lv.SubItems.Add(dr("empposition"))
+            ListView1.Items.Add(lv)
+        Loop
+
+    End Sub
+
+End Class
